4db3a3cd9a035e2ed39816b7e96815ba738116c2
Alejandra Durán <alejandra.duran.marquez@gmail.com>
2016-12-03 20:58:47 -0600

    Estructura, contenido y 1 de 4 puntos de quiebre

---
+++ js/main.js
@@ -0,0 +1,61 @@
+
+$(document).ready(function(){
+	// menu
+	$('.navicon').on('click', function(e) {
+		e.preventDefault();
+		$(this).toggleClass('navicon--active');
+		$('.toggle').toggleClass('toggle--active');
+	});
+
+	  $("#myCarousel").carousel({interval: 2000});
+    
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+        $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#myCarousel").carousel(1);
+    });
+    $(".item3").click(function(){
+        $("#myCarousel").carousel(2);
+    });
+    $(".item4").click(function(){
+        $("#myCarousel").carousel(3);
+    });
+    $(".item5").click(function(){
+        $("#myCarousel").carousel(4);
+    });
+    // Enable Carousel Controls
+    $(".left").click(function(){
+        $("#myCarousel").carousel("prev");
+    });
+    $(".right").click(function(){
+        $("#myCarousel").carousel("next");
+    });
+
+    jQuery('.skillbar').each(function(){
+        jQuery(this).find('.skillbar-bar').animate({
+            width:jQuery(this).attr('data-percent')
+        },6000);
+    });
+});
+
+//caratula
+
+(function() {
+    var lineMaker = new LineMaker({
+            parent: { element: document.body, position: 'append' },
+            position: 'fixed',
+            lines: [
+                {top: 0, left: '0', width: '20vw', height: '100vh', color: '#F8D9E8', hidden: false, animation: { duration: 1000, easing: 'easeInOutCirc', delay: 0, direction: 'TopBottom' }},
+                {top: 0, left: '20vw', width: '20vw', height: '100vh', color: '#fff', hidden: false, animation: { duration: 1000, easing: 'easeInOutCirc', delay: 150, direction: 'BottomTop' }},
+                {top: 0, left: '40vw', width: '20vw', height: '100vh', color: '#F8D9E8', hidden: false, animation: { duration: 1000, easing: 'easeInOutCirc', delay: 300, direction: 'TopBottom' }},
+                {top: 0, left: '60vw', width: '20vw', height: '100vh', color: '#fff', hidden: false, animation: { duration: 1000, easing: 'easeInOutCirc', delay: 450, direction: 'BottomTop' }},
+                {top: 0, left: '80vw', width: '20vw', height: '100vh', color: '#F8D9E8', hidden: false, animation: { duration: 1000, easing: 'easeInOutCirc', delay: 600, direction: 'TopBottom' }}
+            ]
+    });
+    
+    setTimeout(function() {
+        lineMaker.animateLinesOut();
+    }, 500);
+})();
